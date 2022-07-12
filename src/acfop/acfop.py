cf73cc66d7d30ca0ec5745d75cc84c5569d11715
Nico Coetzee <nicc777@gmail.com>
2022-07-12 04:19:57 +0000

    refactor: added comments for main steps in main() function and improved result logging

--- src/acfop/acfop.py
+++ src/acfop/acfop.py
@@ -7,6 +7,7 @@
 """
 
 import sys
+import json
 from acfop.utils import get_logger
 from acfop.utils.cli_arguments import parse_command_line_arguments
 from acfop import BUILD_ID
@@ -14,14 +15,22 @@ from acfop.models.runtime import Variable, VariableStateStore
 
 
 def main(cli_args: list=sys.argv[1:])->dict:
+    # Start the build
     state_store = VariableStateStore()
     state_store.add_variable(var=Variable(id='build_id', initial_value=BUILD_ID, value_type=str))
     logger = get_logger()
     logger.info('Started with build ID {}'.format(state_store.get_variable_value(id='build_id')))
+    
+    # Parse command line arguments (update state_store)
     state_store = parse_command_line_arguments(cli_args=cli_args, state_store=state_store, logger=logger)
+    
+    # Prepare the final result
     result = dict()
     result['BuildId'] = state_store.get_variable_value(id='build_id')
     result['SourceConfigFile'] = state_store.get_variable_value(id='args:config_file')
+
+    # Log and return final result
+    logger.info('RESULT: {}'.format(json.dumps(result, indent=4, sort_keys=True, default=str)))
     return result
 
 
