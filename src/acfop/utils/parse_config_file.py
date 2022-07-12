f7ec462797cf7dccecd0371c3544effe8c3b10f4
Nico Coetzee <nicc777@gmail.com>
2022-07-12 04:32:40 +0000

    feat: created initial configuration parser source file

---
+++ src/acfop/utils/parse_config_file.py
@@ -0,0 +1,17 @@
+"""
+    Copyright (c) 2022. All rights reserved. NS Coetzee <nicc777@gmail.com>
+
+    This file is licensed under GPLv3 and a copy of the license should be included in the project (look for the file 
+    called LICENSE), or alternatively view the license text at 
+    https://raw.githubusercontent.com/nicc777/acfop/main/LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt
+"""
+
+
+from acfop.utils import get_logger
+from acfop.models.runtime import Variable, VariableStateStore
+
+
+def update_state_store_from_config_file(state_store: VariableStateStore, logger=get_logger())->VariableStateStore:
+
+    return state_store
+
