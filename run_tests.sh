8b45a6b2c40fbf2fb3ff68db906e2239445c8d42
Nico Coetzee <nicc777@gmail.com>
2022-07-12 04:42:27 +0000

    test: updated tests

--- run_tests.sh
+++ run_tests.sh
@@ -11,5 +11,6 @@ coverage run -a tests/test_aws.py
 coverage run -a tests/test_functions.py
 coverage run -a tests/test_functions_aws_helpers.py
 coverage run -a tests/test_utils_cli_arguments.py
+coverage run -a tests/test_utils_parse_config_file.py
 
 coverage report --omit="tests/test*" -m
