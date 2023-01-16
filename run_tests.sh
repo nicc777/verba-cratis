38152dd8463de81255baa7f485071a4f985be369
Nico Coetzee <nicc777@gmail.com>
2023-01-16 07:51:55 +0100

    test: adding docker echo server to tests to test notifications

--- run_tests.sh
+++ run_tests.sh
@@ -4,16 +4,16 @@
 
 docker run --name echo_server -d -p 8089:80 kennethreitz/httpbin
 
-coverage run tests/test_utils_file_io.py
+# coverage run tests/test_utils_file_io.py
 # coverage run -a tests/test_utils_parser.py
 coverage run -a tests/test_utils_parser2.py
 # coverage run -a tests/test_utils.py
-coverage run -a tests/test_verbacratis.py
+# coverage run -a tests/test_verbacratis.py
 # coverage run -a tests/test_models_runtime.py
-coverage run -a tests/test_aws.py
-coverage run -a tests/test_functions.py
-coverage run -a tests/test_functions_aws_helpers.py
-coverage run -a tests/test_utils_cli_arguments.py
+# coverage run -a tests/test_aws.py
+# coverage run -a tests/test_functions.py
+# coverage run -a tests/test_functions_aws_helpers.py
+# coverage run -a tests/test_utils_cli_arguments.py
 # coverage run -a tests/test_utils_parse_config_file.py
 
 coverage report --omit="tests/test*" -m
