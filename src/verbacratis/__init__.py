ecfe60b182ea9f93931794c3c5502bfdf7a21a4b
Nico Coetzee <nicc777@gmail.com>
2023-01-17 07:33:00 +0100

    feat: added get_directory_from_path()

--- src/verbacratis/__init__.py
+++ src/verbacratis/__init__.py
@@ -15,6 +15,39 @@ from sqlalchemy import create_engine
 from verbacratis.utils import get_logger
 
 
+def get_directory_from_path(input_path: str)->str:
+    """Returns the directory portion of a path
+    
+    The function first tests if the path exists. If it does not exist, the assumption is that the path consists of a directory and filename.
+
+    If the path does exist, it will be determined if it is a directory or a normal file. Only the directory portion will be returned.
+
+        Args:
+            path: (required) String of a full path (directory and/or filename)
+
+        Returns:
+            A STRING with the calculated directory portion of the input path
+    """
+    p = Path(input_path)
+    if p.exists() is True:
+        if p.is_dir() is True:
+            input_path = '{}{}dummy'.format(
+                input_path,
+                os.sep
+            )
+    elements = input_path.split(os.sep)
+    if len(elements) > 0:
+        elements.pop(0)
+    if len(elements) == 1:
+        return '/'
+    final_path = '{}'.format(os.sep).join(elements[:-1])
+    final_path = '{}{}'.format(
+        os.sep,
+        final_path
+    )
+    return final_path
+
+
 class ApplicationState:
 
     def __init__(self) -> None:
--- src/verbacratis/utils/cli_arguments.py
+++ src/verbacratis/utils/cli_arguments.py
@@ -12,6 +12,45 @@ import argparse
 from verbacratis import ApplicationState
 
 
+def _get_arg_parser(
+    default_config_file: str,
+    default_environment: str,
+    default_project: str
+)->argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description='Processes and execute an AWS CloudFormation deployment based on the supplied configuration')
+    parser.add_argument(
+        '-c', '--conf',
+        action='store',
+        nargs='*',
+        dest='config_file',
+        metavar='CONFIGURATION_FILE',
+        type=str, 
+        default=default_config_file,
+        help='The path and filename of the configuration file. REQUIRED'
+    )
+    parser.add_argument(
+        '-e', '--env',
+        action='store',
+        nargs='*',
+        dest='environment',
+        metavar='ENVIRONMENT_NAME',
+        type=str, 
+        default=default_environment,
+        help='The environment name (used to identify tasks and deployments from all manifest files)'
+    )
+    parser.add_argument(
+        '-p', '--project',
+        action='store',
+        nargs='*',
+        dest='project',
+        metavar='PROJECT_NAME',
+        type=str, 
+        default=default_project,
+        help='The project name (used in reporting only)'
+    )
+    return parser
+
+
 def parse_command_line_arguments(
     state:ApplicationState,
     cli_args: list=sys.argv[1:],
@@ -30,20 +69,21 @@ def parse_command_line_arguments(
     state.logger.info('cli_args={}'.format(cli_args))
     args = dict()
     args['conf'] = None
-    parser = argparse.ArgumentParser(description='Processes and execute an AWS CloudFormation deployment based on the supplied configuration')
-    parser.add_argument(
-        '-c', '--conf',
-        action='store',
-        nargs='*',
-        dest='config_file',
-        metavar='CONFIGURATION_FILE',
-        type=str, 
-        help='The path and filename of the configuration file. REQUIRED'
+    parser = _get_arg_parser(
+        default_config_file=state.cli_args['config_file'],
+        default_environment=state.environment,
+        default_project=state.project
     )
     parsed_args, unknown_args = parser.parse_known_args(cli_args)
 
     if parsed_args.config_file is not None:
         args['config_file'] = parsed_args.config_file[0]
+
+    if parsed_args.environment is not None:
+        state.environment =  parsed_args.environment[0]
+
+    if parsed_args.project is not None:
+        state.environment =  parsed_args.project[0]
 
     for k,v in overrides.items():
         args[k] = v
