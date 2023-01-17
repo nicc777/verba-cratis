e309e234b5f72f483ced0b6e5ba3138908b560d8
Nico Coetzee <nicc777@gmail.com>
2023-01-17 09:49:51 +0100

    refactor: cleanup main entry point code

--- src/verbacratis/verbacratis.py
+++ src/verbacratis/verbacratis.py
@@ -11,8 +11,6 @@ import json
 from verbacratis.utils import get_logger
 from verbacratis.utils.cli_arguments import parse_command_line_arguments
 from verbacratis import ApplicationState
-# from verbacratis.models.runtime import Variable, VariableStateStore
-# from verbacratis.utils.parser import parse_configuration_file
 
 
 def main(cli_args: list=list(), logger=get_logger())->dict:
@@ -21,24 +19,9 @@ def main(cli_args: list=list(), logger=get_logger())->dict:
     ###
     ### Start the build
     ###
-    # state_store = VariableStateStore()
-    # state_store.add_variable(var=Variable(id='build_id', initial_value=BUILD_ID, value_type=str))
     state: ApplicationState = parse_command_line_arguments(state=ApplicationState(), cli_args=cli_args, logger=logger)
     state.logger.info('Started with build ID {}'.format(state.build_id))
     
-    ###
-    ### Read and parse the configuration
-    ###
-    #configuration_as_dict = parse_configuration_file(file_path=state_store.get_variable_value(id='args:config_file'))
-
-    ###
-    ### Prepare the final result
-    ###
-    #result = dict()
-    #result['BuildId'] = state_store.get_variable_value(id='build_id')
-    #result['SourceConfigFile'] = state_store.get_variable_value(id='args:config_file')
-    #result['RuntimeConfiguration'] = configuration_as_dict
-
     ###
     ### Log and return final result
     ###
