aa341e2c5c3d824feff601bfc883e0cd3e0a70c6
Nico Coetzee <nicc777@gmail.com>
2023-01-26 06:28:33 +0100

    feat: Added features to Projects

--- src/verbacratis/models/runtime_configuration.py
+++ src/verbacratis/models/runtime_configuration.py
@@ -168,6 +168,15 @@ class Projects(Items):
 
     def add_project(self, project: Project):
         self.add_item(item=project)
+        for environment_name in project.scopes:
+            if environment_name not in self.project_names_per_environment:
+                self.project_names_per_environment[environment_name] = list()
+                self.project_names_per_environment[environment_name].append(project.name)
+
+    def get_project_names_for_named_environment(self, environment_name: str='default')->list:
+        if environment_name in self.project_names_per_environment:
+            return self.project_names_per_environment[environment_name]
+        raise Exception('Environment named "{}" not found in collection of projects'.format(environment_name))
         
 
 class InfrastructureAccount:
