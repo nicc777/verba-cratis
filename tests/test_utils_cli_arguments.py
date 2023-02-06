8036815c7fa3c28ea2ccc18729c377742977da71
Nico Coetzee <nicc777@gmail.com>
2023-02-06 06:58:55 +0100

    test: adding more tests

--- tests/test_utils_cli_arguments.py
+++ tests/test_utils_cli_arguments.py
@@ -58,7 +58,7 @@ class TestFunctionParseCommandLineArguments(unittest.TestCase):  # pragma: no co
 
     def test_basic_invocation_args_complex_git_location(self):
         result = parse_command_line_arguments(state=ApplicationState(), cli_args=self.cli_args_complex)
-        self.config_dir = result.config_directory
+        # self.config_dir = result.config_directory
         result.update_config_file(
             config_file='{}{}verbacratis.yaml'.format(
                 self.config_dir,
