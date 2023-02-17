448ad67491533ac77c3fb47ec17ed8cd3351725f
Nico Coetzee <nicc777@gmail.com>
2023-02-17 05:20:44 +0100

    test: updates tests

---
+++ clean_tmp.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+rm -frR /tmp/Temp-*  || true
+rm -frR /tmp/Location__*  || true
+rm -frR /tmp/test_single_local_file  || true
+rm -frR /tmp/test_files  || true
+rm -frR /tmp/test_git_clone  || true
+rm -frR /tmp/test  || true
+rm -frR /tmp/copied_file  || true
+rm -frR /tmp/sample-docker-app  || true
+rm -frR /tmp/local-docker-install  || true
+rm -frR /tmp/test-project  || true
+
